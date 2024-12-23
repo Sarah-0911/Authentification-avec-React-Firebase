b4100986907e34a7cca7bce5621ead147d7a9ee2
Sarah-0911 <sarah.tabet69@gmail.com>
2024-12-23 17:49:46 +0100

    Route privée ok (update)

--- src/pages/Private/PrivateHome/PrivateHome.jsx
+++ src/pages/Private/PrivateHome/PrivateHome.jsx
@@ -2,9 +2,9 @@ import catAtDesk from "./catAtDesk.webp"
 
 export default function PrivateHome() {
   return (
-    <div className="w-[400px] mx-auto">
-      <h1 className="text-green-400 font-semibold">Home Sweet private Home</h1>
-      <img src={catAtDesk} alt="cat at desk gif" />
+    <div className="flex flex-col justify-center items-center w-[400px] mx-auto">
+      <h1 className="text-green-400 font-semibold my-4">Home Sweet private Home</h1>
+      <img className="w-[80%]" src={catAtDesk} alt="cat at desk gif" />
     </div>
   )
 }
